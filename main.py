146a74b4bfeab7cad99da984f578573f0bd5ed30
CTRL-B-X <155469656+CTRL-B-X@users.noreply.github.com>
2024-05-10 14:36:55 -0400

    Update to main.py

--- main.py
+++ main.py
@@ -6,10 +6,21 @@ import sys
 
 
 def main():
-        book_path = "books/frankenstein.txt"
-        text = get_book_text(book_path)
+        book_pat = "books/frankenstein.txt"
+        text = get_book_text(book_pat)
         num_words = get_num_words(text)
+        chars_dict = get_chars_dict(text)
+        chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+        
+        print(f"--- Begin report of {book_pat} ---")
         print(f"{num_words} words found in the document")
+        print()
+
+        for item in chars_sorted_list:
+            if not item["char"].isalpha():
+                continue
+            print(f"The '{item['char']}' characrter was found {item['num']} times")
+        print("--- End report ---")
             
 
 
@@ -19,12 +30,29 @@ def get_num_words(text):
     words = text.split()
     return len(words)
 
-def get_book_text(path):
-    with open(path) as f:
+def sort_on(d):
+    return d["num"]
+
+def get_book_text(pat):
+    with open(pat) as f:
         return f.read()
 
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
 
-
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
 
 
 
